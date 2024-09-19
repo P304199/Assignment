4d19610813a350a3b9b5ad69a4275b3cc4b4e6e7
P304199 <praghya975@gmail.com>
2024-09-19 14:06:59 +0530

    Add files via upload

---
+++ q24.c
@@ -0,0 +1,36 @@
+/* 24. WRITE A C PROGRAM TO FIND THE VALUE OF Y USING
+1+x where n=1
+1+x/n where n=2
+ Y(x,n) = 1 +xn when n=3
+1+nx when : n>3 or n<1
+*/
+
+#include <stdio.h>
+#include<math.h>
+int main ()
+{
+    int y,n,x; // declaration of variables
+
+    //Taking input from the user
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+     printf("Enter the value of x: ");
+     scanf("%d",&x);
+
+    //Using switch statement to get the value of y as per the value of n
+    switch(n)
+    {
+        case 1 : y = 1+x;
+        break;
+        case 2 : y = 1 + x/n;
+        break;
+        case 3: y = 1 +pow(x,n);
+        break;
+        default : y = 1 +n*x;
+        break;
+    }
+
+    printf("%d",y);
+    return 0;
+
+}
