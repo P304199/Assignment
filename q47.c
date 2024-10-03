a6c61df79a26f174147f98794b1cd629fd408c43
P304199 <praghya975@gmail.com>
2024-10-03 16:21:12 +0530

    Update q47.c

--- q47.c
+++ q47.c
@@ -9,11 +9,10 @@ ABCD
 #include<stdio.h>
 int main()
 {
-    int n;//declaring variable to store user's input
-    //taking input from the user
+    int n;
     printf("Enter the value of n:");
     scanf("%d",&n);
-    //printing pattern using nested loops
+    
     for(char i = 1 ; i <=  n; i++)
     {
         for(char j ='A'; j < 65+i; j++)
