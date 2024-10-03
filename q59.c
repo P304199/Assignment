cb59c473cd9314b89f5653827a7542d7d999f814
P304199 <praghya975@gmail.com>
2024-10-03 16:33:18 +0530

    Update q59.c

--- q59.c
+++ q59.c
@@ -2,10 +2,10 @@
 #include<stdio.h>
 int main()
 {
-    int arr1[10];   //declaration of array1 of size 10
-    int arr2[10];   //declaration of array2 of size 10
+    int arr1[10];  
+    int arr2[10];   
     
-    //taking input from the user to store data in array 1
+   
     printf("Enter the data for array 1\n");
     
     for(int i =0; i <10; i++)
@@ -13,7 +13,7 @@ int main()
         printf("Enter number:");
         scanf("%d",&arr1[i]);
     }
-    //taking input from the user to store data in array 2
+    
     printf("Enter the data for array 2\n");
 
     for(int i =0; i <10; i++)
@@ -22,7 +22,7 @@ int main()
         scanf("%d",&arr2[i]);
 
     }
-    //swapping 
+    
     for(int i =0; i <10; i++)
     {
         int swap;
@@ -30,7 +30,7 @@ int main()
         arr1[i] = arr2[i];
         arr2[i] = swap;
     }
-        //printing values after swapping
+        
        printf("Values after swapping\n");
     for(int i =0; i <10; i++)
     {
