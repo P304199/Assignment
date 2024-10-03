265b55e52b574c2d9b0b63a4a7eb75024ddc0fc1
P304199 <praghya975@gmail.com>
2024-10-03 16:23:55 +0530

    Update q50.c

--- q50.c
+++ q50.c
@@ -9,22 +9,21 @@ EEEEE
 */
 
 
-#include<stdio.h> //inclusion of standard input output header file
+#include<stdio.h> 
 int main()
 {   
-    int n; // declaring variable for storing user's input
-
-    // taking input from the user
+    int n; 
+    
     printf("Enter the value of n: ");
     scanf("%d",&n);
-//triggering the loop for printing the required pattern
+
     for(char i ='A'; i <=65+n; i++)
     {
         for(int j =65 ; j<=i;j++ )
         {
-            printf("%c",i);//printing the value of variable i
+            printf("%c",i);
         }
-        printf("\n");//printing new line
+        printf("\n");
     }
     return 0;
 }
