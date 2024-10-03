2cc1d2b67fbd144398af149e9a23ebd9a80c03fb
P304199 <praghya975@gmail.com>
2024-10-03 16:29:48 +0530

    Update q56.c

--- q56.c
+++ q56.c
@@ -4,17 +4,17 @@
 #include <time.h>
 int main ()
 {
- int arr[10]; //declaration of an array of size 10
+ int arr[10];
 
- int poscount =0; //declaration of variable to store the count of positive numbers and intializing with value 0
- int negcount =0;//declaration of variable to store the count of negative numbers and intializing with value 0
- int zerocount =0;//declaration of variable to store the count of zeroes and intializing with value 0
+ int poscount =0;
+ int negcount =0;
+ int zerocount =0;
 
- for(int i =0; i <10; i++) //triggering the loop to store the input in array
- {  //taking input from the user
+ for(int i =0; i <10; i++) 
+  {
     printf("Enter number: ");
     scanf("%d",&arr[i]);
-    //counting positive numbers, negative numbers and zeroes
+    
     if(arr[i]==0)
     zerocount ++;
 
@@ -26,7 +26,7 @@ int main ()
 
 
  } 
-//printing output
+
  printf("You have entered %d zeroes, %d positive numbers and %d negative numbers.",zerocount,poscount,negcount) ;
  return 0;
 }
