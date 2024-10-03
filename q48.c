45758ff18b626ddad40b1e4850e7272d74bd357d
P304199 <praghya975@gmail.com>
2024-10-03 16:22:22 +0530

    Update q48.c

--- q48.c
+++ q48.c
@@ -9,18 +9,17 @@
 #include<stdio.h>
 int main()
 {
-    int n; //declaration of variable to store the user's input
-  int count =1; //declaration and initialization of variable count with value 1
-    //taking input from the user
+    int n; 
+  int count =1; 
     printf("Enter the value of n:");
     scanf("%d",&n);
-    //triggering nested loops for printing the pattern
+    
 
-    for(int i =1; i <= n; i ++) //loop for printing rows
+    for(int i =1; i <= n; i ++) 
     {
-        for(int j =1; j <=n; j++) //loop for printing columns
+        for(int j =1; j <=n; j++) 
         {
-            if(i+j >n) //pattern condition
+            if(i+j >n) 
             {
                 printf("%d",count);
 
